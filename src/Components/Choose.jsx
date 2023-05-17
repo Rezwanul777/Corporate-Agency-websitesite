548f1ab43e161d672d29b4941ab24ad54ce7109d
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-05-17 15:43:25 +0600

    choose component

--- src/Components/Choose.jsx
+++ src/Components/Choose.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import chooseImg from '../assets/images/choose.jpg.jpeg'
+import * as Icon from 'react-bootstrap-icons';
+
 
 const Choose = () => {
   return (
@@ -8,20 +10,24 @@ const Choose = () => {
        <section id='choose-section'>
        <Container className='mt-5'>
             <div className="row">
+           
                 <div className="col-12 col-lg-6">
                    <div data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">
                    <div className="header-text">
-                    <h1 className='mt-2 text-primary'>Why choose us?</h1>
+                   <h1 className='text-primary '>Why choose us?</h1>
                     <h3 className='mt-2'>Our approach is pragmatic and supportive for  rigorous technical demands and integrity of accredited certification.</h3>
                     </div>
                    <div className="body-text" >
-                   <p>
-                      We are helpful and courteous to customers, and our team is dedicated to delivering excellent service based on understanding their needs.   
-                    </p>
-                    <p>We aim for a culture of continuous improvement, reviewing and improving our work to add value. </p>
-                    <p>
-                      We constructively review client and staff feedback to reach our full potential and consistently deliver excellent services. We value and encourage professional development by providing support and learning opportunities to enable everyone to develop to their full potential.
-                    </p>
+                   <div className="py-3 feature-text">
+                   <p><Icon.ArrowRight size={30} style={{ 'color': "blue" }} />	Improvement focus</p>
+                        <p><Icon.ArrowRight size={30} style={{ 'color': "blue" }}/>Practical advice...</p>
+                        <p><Icon.ArrowRight size={30} style={{ 'color': "blue" }}/>Comprehensive service</p>
+                        <p><Icon.ArrowRight size={30} style={{ 'color': "blue" }}/>Value for money</p>
+                        <p><Icon.ArrowRight size={30} style={{ 'color': "blue" }}/>The latest technical advice</p>
+                       
+
+                    
+                  </div>
                    </div>
                    </div>
                 </div>
